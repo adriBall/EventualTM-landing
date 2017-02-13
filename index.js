c87e6d4c251b667a6192f0f6d8e70e919b5dcdd2
adriBall <al286288@uji.es>
2017-02-13 21:29:32 +0100

    Cambiando cosas

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
+app.set('port', (process.env.PORT || 5000));
 
 app.engine('.hbs', exphbs({  
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
+
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))  
 app.use(express.static(__dirname + '/public'));
@@ -18,4 +20,6 @@ app.get('/', (request, response) => {
   response.render('home')
 })
 
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
