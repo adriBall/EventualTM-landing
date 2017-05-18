7b5a1aea0296801121d32783416326a7b848107a
adriBall <al286288@uji.es>
2017-05-18 21:00:56 +0200

    Añadidas secciones

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 // index.js
-const path = require('path')  
-const express = require('express')  
-const exphbs = require('express-handlebars')
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-const app = express()
+const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.engine('.html', exphbs({  
   defaultLayout: 'main',
   extname: '.html',
   layoutsDir: path.join(__dirname, 'views/layouts')
-}))
+}));
 
 app.set('view engine', '.html');
-app.set('views', path.join(__dirname, 'views'))  
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {  
-  response.render('home')
-})
+app.get('/', (request, response) => response.render('home'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
